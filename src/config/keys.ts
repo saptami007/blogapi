15954aa0f9df00d8ce96ea0810e1ea44d1cde516
Subhadeep Mondal <subhadeepmondal@Subhadeeps-MacBook-Pro.local>
2023-03-12 00:51:40 +0530

    removing keys

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default{
-    mongoURI:'mongodb+srv://admin1111:admin1111@cluster0.a5qfln7.mongodb.net/?retryWrites=true&w=majority',
-    jwtKey:'SECRET@123'
+    mongoURI:'',
+    jwtKey:''
 }
